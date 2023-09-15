707deb2e4e23bf2f2ad39896f51f291ae293bd43
André Leodoro <aleodoro93@gmail.com>
2023-09-15 16:30:06 -0300

    criação da model de clientes

---
+++ src/repository/models/clientesModels.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Clientes = mongoose.model('Funcionario',{
+    id_CPF: String, 
+    nome: String,
+    endereco: String,
+    telefone: String,
+    email: String
+})
+
+export default Clientes;   
+
