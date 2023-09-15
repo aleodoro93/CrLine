845fedd24b78691e8cbb7f1e629b84bc9fd02dbb
Juliane <julianebueno1@gmail.com>
2023-09-14 22:46:20 -0300

    feat: conectando o mongodb e protegendo o .env

---
+++ src/repository/Repository.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+class Repository{
+    static async create(model, input){
+        await model.create(input)
+        return {Sucesso: "Sucesso no registro!"}
+    }
+
+    static async findAll(model){
+        const response = await model.find()
+        return response
+    }
+
+    static async findById(model){
+        const response = await model.findOne({_id: id})
+        return response
+    }
+
+    static async updateById(model, id, input){
+        await model.findOne({_id: id}, input)
+        return {message: "Sucesso na atualização de registro", id}
+    }
+    
+    static async deleteById(model, id){
+        await model.findOneAndDelete({_id: id})
+        return {message: "Sucesso na exclusão do registro", id}
+    }
+    
+    static async deleteByKey(model, key, value){
+        const response = await model.findOne({[key]:value})
+        return response
+    }
+}
+
+export default Repository
