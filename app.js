cd565d876c7c4126cea824dc147fdf474f91b1dd
André Leodoro <aleodoro93@gmail.com>
2023-09-14 22:08:59 -0300

    feat: instalação das dependencias e criação do app e levante do servidor.

---
+++ app.js
@@ -0,0 +1,10 @@
+import express  from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Servidor online em http://localhost:${port}`)
+});
+
